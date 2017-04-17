2307c1f465c4d6e44310b64552b433269a1b365d
reeleelq <reeleelq@gmail.com>
2017-04-18 01:19:57 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
       @groups = Group.all
   end
@@ -8,7 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+
   end
 
 
@@ -18,6 +20,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_path
@@ -27,7 +30,7 @@ class GroupsController < ApplicationController
   end
 
     def update
-      @group = Group.find(params[:id])
+
       if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
       else
@@ -36,7 +39,7 @@ class GroupsController < ApplicationController
     end
 
    def destroy
-     @group = Group.find(params[:id])
+
      @group.destroy
      flash[:alert] = "Group deleted"
      redirect_to groups_path
@@ -44,8 +47,16 @@ class GroupsController < ApplicationController
 
    private
 
-   def group_params
-     params.require(:group).permit(:title, :description)
-   end
+   def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 
 end
